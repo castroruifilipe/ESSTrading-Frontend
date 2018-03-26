bb7659ac3c6f0c5ee6803915106c73ae417b6629
JTGomes <tiago.gomez@hotmail.com>
2018-03-26 15:35:53 +0100

    navBar

---
+++ ESS/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './scenes/Home'
+import Login from './scenes/Login'
+
+class App extends Component {
+  render() {
+    return (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
+     </Switch>
+  </BrowserRouter>
+
+    );
+  }
+}
+
+export default App;
---
+++ ESS/src/scenes/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from '../../components/NavBar/';
+
+class Home extends Component {
+
+  render(){
+    return(
+      <NavBar />
+    );
+  }
+
+}
+
+export default Home;
