8ebc72931bf95c630e046e35bd4aae14d43193ba
CarvalhaisMatos <carvalhaismatos@gmail.com>
2018-03-28 11:03:04 +0100

    Update footer, sempre em baixo

--- ESS/src/components/App.js
+++ ESS/src/components/App.js
@@ -15,18 +15,22 @@ class App extends Component {
 
 	render() {
 		return (
-			<Router>
-				<div>
-					<NavBar />
-					<hr class="mt-0 mb-0" />
+			<Router >
+				<div id="wrapper">
+					<div id="content">
+						<NavBar />
+						<hr class="mt-0 mb-0" />
 
-					<Route exact path={routes.HOME} component={Home}/>
-					<Route exact path={routes.LOGIN} component={Login}/>
-					<Route exact path={routes.REGISTAR} component={Registar}/>
-					<Route exact path={routes.SOBRE} component={Sobre}/>
-
-					<Footer />
+						<Route exact path={routes.HOME} component={Home}/>
+						<Route exact path={routes.LOGIN} component={Login}/>
+						<Route exact path={routes.REGISTAR} component={Registar}/>
+						<Route exact path={routes.SOBRE} component={Sobre}/>
+					</div>
+					<div id="footer">
+						<Footer />
+					</div>
 				</div>
+
 			</Router>
 		);
 	}
