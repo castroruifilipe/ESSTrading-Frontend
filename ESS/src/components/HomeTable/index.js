eeeb0f227250222a5460e35dd371d9b8d1f17fb5
JTGomes <tiago.gomez@hotmail.com>
2018-03-28 19:34:46 +0100

    add botao da variaçao na tabela

--- ESS/src/components/HomeTable/index.js
+++ ESS/src/components/HomeTable/index.js
@@ -1,40 +1,36 @@
 import React, { Component } from 'react';
 import Table from '../Table'
+import ButaoVariacao from './components/ButaoVariacao'
 
 class HomeTable extends Component {
 	constructor(props){
 		super(props);
 		this.state={
-			header: [
-				'Ativo',
-				<div className="dropdown text-center">
-					<button className="btn btn-light dropdown-toggle"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Variação
-					</button>
-					<div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-						<button className="dropdown-item" type="button">1H</button>
-						<button className="dropdown-item" type="button">4H</button>
-						<button className="dropdown-item" type="button">1D</button>
-					</div>
-				</div>
-				,'Venda($)',
+			header: ['Ativo',
+				<ButaoVariacao  onChangeVariacao={(variacao)=> this.changeVariacao(variacao)} />,
+				'Venda($)',
 				'Compra($)'
 			],
 			rows: [{ativo: 'AMZN',variacao: '0.48%', venda: 1542.61, compra: 1545.74},
 						 {ativo: 'AAPL', variacao: '-0.93%', venda: 174.67, compra: 175.01}
 					 ]
-				 }
+		};
+
 	}
 
-updateRows(){
-	this.setState();
-}
+	changeVariacao(variacao){
+		console.log(variacao +' from HomeTable');
+	}
+
+	updateRows(){
+		this.setState();
+	}
 
 	render() {
 		return (
-				<Table
-					header={this.state.header}
-					rows={this.state.rows}
+			<Table
+				header={this.state.header}
+				rows={this.state.rows}
 				/>
 		);
 	}
