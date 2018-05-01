b83b264de5260f7711f286ef29b09cfd0419cc98
DiogoMachadoA75399 <DiogoMachadoA75399>
2018-05-01 17:29:59 +0100

    improve about

--- ESS/src/scenes/Sobre/index.js
+++ ESS/src/scenes/Sobre/index.js
@@ -13,6 +13,13 @@ const k2Url = require("../../images/k2.jpg");
 const k3Url = require("../../images/k3.jpg");
 const k4Url = require("../../images/k4.jpg");
 
+const HTML = require("../../images/html5.jpg");
+const CSS = require("../../images/CSS.3.svg.png");
+const JS = require("../../images/javascript.png");
+const BootstrapLogo = require("../../images/bootstrap.jpg");
+const FireBaseLogo = require("../../images/firebase.png");
+const ReactLogo = require("../../images/react.jpg");
+
 class Sobre extends Component {
 	render() {
 		return (
@@ -23,15 +30,42 @@ class Sobre extends Component {
 							<Row className="no-gutters">
 								<div className="col-lg-6 order-lg-2 text-white showcase-img" style={imageUrl(diUrl)}></div>
 								<div className="col-lg-6 order-lg-1 my-auto showcase-text">
-									<h2>Projecto de Engenharia Web</h2>
-									<p className="lead mb-0">Uma correctora online que permite aos utilizadores brincarem à compra e venda de CFDs!</p>
+									<h2 className="pb-3">Projecto de Engenharia Web</h2>
+									<p className="lead mb-0 text-justify">
+										A ESS Online Trading Platform é uma plataforma de negociação que permite a investidores e
+										traders abrir, fechar e gerir posições no mercado finaneiro, envolvendo a compra a e venda
+										de ativos financeiros, nomeadamente a compra de ações, commodities, índices e moedas.
+									</p>
 								</div>
 							</Row>
 							<Row className="no-gutters">
 								<div className="col-lg-6 text-white showcase-img" style={imageUrl(sobreUrl)}></div>
-								<div className="col-lg-6 my-auto showcase-text">
+								<div className="col-lg-6 my-auto showcase-text" style={{padding:'70px 70px'}}>
 									<h2>Tecnologias</h2>
-									<p className="lead mb-0">React, HTML, CSS, Javascript e Bootstrap.</p>
+									<p className="lead"> HTML, CSS, Javascript, React, Bootstrap e Firebase.</p>
+									<Row>
+										<Col className="py-3">
+											<img src={HTML} alt="HTML5" style={{width:'100%'}}/>
+										</Col>
+										<Col className="py-3">
+											<img src={CSS} alt="CSS3" style={{width:'100%'}}/>
+										</Col>
+										<Col className="py-3">
+											<img src={JS} alt="JS" style={{width:'100%'}}/>
+										</Col>
+									</Row>
+									<Row>
+										<Col className="py-3">
+											<img src={BootstrapLogo} alt="Bootstrap" style={{width:'100%'}}/>
+										</Col>
+										<Col className="py-3">
+											<img src={ReactLogo} alt="React" style={{width:'100%'}}/>
+										</Col>
+										<Col md={{offset:1}} className="py-3">
+											<img src={FireBaseLogo} alt="Firebase" style={{maxWidth:'173px', maxHeight:'150px'}}/>
+										</Col>
+									</Row>
+
 								</div>
 							</Row>
 						</section>
