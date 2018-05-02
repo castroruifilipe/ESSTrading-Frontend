0858d24e863eb57d3c523aba5ab601cb0dc88486
Rui Leite <a75551@alunos.uminho.pt>
2018-05-02 19:27:57 +0100

    Home

--- ESS/src/scenes/Home/index.js
+++ ESS/src/scenes/Home/index.js
@@ -10,34 +10,36 @@ class Home extends Component {
 
 	render() {
 		return (
-			<div>
-				<section className="showcase" style={{ marginBottom: '30px' }}>
-				<Container fluid style={{ paddingRight: '0', paddingLeft: '0' }}>
-						<Row className="no-gutters">
-							<Col lg='12' className="order-lg-3 showcase-img"
-								style={{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' + Background + ')' }}>
+			<Container fluid style={{ paddingRight: '0', paddingLeft: '0' }}>
+				<Row className="no-gutters">
+					<Col>
+						<section className="mb-0 showcase showcase-img" style={{
+							height: '100vh',
+							backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' + Background + ')',
+							alignItems: 'center',
+							display: 'flex',
+						}}>
+							<div className="text-white col-lg-6" style={{ padding: '0px 60px', textShadow: '0 0 30px black' }}>
+								<span id="logo-text" style={{ fontWeight: 'bold', fontSize: '40px' }}>
+									ESS Trading
+								</span>
 
-								<div className="text-white col-lg-6" style={{ padding: '100px 30px 0px 100px', textShadow: '0 0 30px black' }}>
-									<span id="logo-text" style={{ fontWeight: 'bold', fontSize: '40px' }}>ESS Trading</span>
-									<h1>Junte-se e ganhe dinheiro com confiança</h1>
-									<span className="lead">
-										Invista em centenas de ativos financeiros e <i>commodities</i> de forma fácil.
-										Poderá ter total controlo sobre os seus negócios.
-										</span>
-									<Link to={routes.REGISTAR}>
-										<Button style={{ margin: '20px 0px 40px 0px' }} color="success" size="lg">
-											REGISTAR
-										</Button>
-									</Link>
-								</div>
-							</Col>
-						</Row>
-					</Container>
-				</section>
-				
-				
+								<h1>Junte-se e ganhe dinheiro com confiança</h1>
+								<span className="lead">
+									Invista em centenas de ativos financeiros e <i>commodities</i> de forma fácil.
+									Poderá ter total controlo sobre os seus negócios.
+								</span>
 
-			</div>
+								<Link to={routes.REGISTAR}>
+									<Button style={{ margin: '20px 0px 40px 0px' }} color="success" size="lg">
+										REGISTAR
+									</Button>
+								</Link>
+							</div>
+						</section>
+					</Col>
+				</Row>
+			</Container>
 		);
 	}
 }
